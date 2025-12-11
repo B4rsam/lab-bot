5d54924e7aad9cc7753a94d0d22471ec73069f67
Barsam Badakhshani <105477761+B4rsam@users.noreply.github.com>
2025-12-11 17:45:39 +0330

    feat: basic access control

--- src/bot.js
+++ src/bot.js
@@ -3,14 +3,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var telegraf_1 = require("telegraf");
 var dotenv_1 = require("dotenv");
 (0, dotenv_1.config)({ path: '.env' });
+var userStore = [{
+        username: process.env.OWNER_USERNAME,
+        id: Number(process.env.OWNER_ID),
+    }];
+var registerLog = [];
 var bot = new telegraf_1.Telegraf(process.env.API_TOKEN);
 bot.start(function (ctx) {
-    ctx.reply('Hello World!');
+    ctx.reply('Lab operator active');
 });
 bot.launch().then(function () {
     console.log('Bot launch');
 }).catch(function (err) {
     console.log(err);
+});
+bot.command("register", function (ctx) {
+    if (!registerLog.includes(ctx.message.from.id)) {
+        ctx.reply("Asking Boss for access...");
+        console.log("New register from id: " + ctx.message.from.id + " with username: " + ctx.message.from.username);
+        registerLog.push(ctx.message.from.id);
+    }
+});
+bot.command("status", function (ctx) {
+    var _a = ctx.message.from, id = _a.id, username = _a.username;
+    if (!!userStore.find(function (user) { return user.id === id && user.username === username; })) {
+        ctx.reply("Access granted");
+    }
+    else {
+        ctx.reply("Access Denied");
+    }
 });
 process.once('SIGINT', function () { return bot.stop('SIGINT'); });
 process.once('SIGTERM', function () { return bot.stop('SIGTERM'); });
--- src/bot.ts
+++ src/bot.ts
@@ -1,18 +1,45 @@
 import { Telegraf } from 'telegraf';
 import { config } from 'dotenv';
 
-config({ path: '.env' })
+config({ path: '.env' });
+
+const userStore: {
+    username: string;
+    id: number;
+}[] = [{
+    username: process.env.OWNER_USERNAME as string,
+    id: Number(process.env.OWNER_ID),
+}];
+
+const registerLog: number[] = [];
 
 const bot = new Telegraf(process.env.API_TOKEN as string);
 
 bot.start((ctx) => {
-    ctx.reply('Hello World!');
+    ctx.reply('Lab operator active');
 });
 
 bot.launch().then(() => {
     console.log('Bot launch');
 }).catch((err) => {
     console.log(err);
+});
+
+bot.command("register", (ctx) => {
+    if (!registerLog.includes(ctx.message.from.id)) {
+        ctx.reply("Asking Boss for access...");
+        console.log("New register from id: " + ctx.message.from.id + " with username: " + ctx.message.from.username);
+        registerLog.push(ctx.message.from.id);
+    }
+});
+
+bot.command("status", (ctx) => {
+    const { id, username } = ctx.message.from
+    if (!!userStore.find((user) => user.id === id && user.username === username)) {
+        ctx.reply("Access granted");
+    } else {
+        ctx.reply("Access Denied");
+    }
 });
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
