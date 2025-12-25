8d50c4ca6055dfdb3ea66f2136115d3ef69c805b
Barsam Badakhshani <105477761+B4rsam@users.noreply.github.com>
2025-12-25 16:07:36 +0330

    wip: docker

--- src/docker.ts
+++ src/docker.ts
@@ -4,40 +4,45 @@ import { config } from "dotenv";
 
 config({ path: ".env" });
 
+const docker = new Api({
+  baseURL: process.env.DOCKER_URL,
+  socketPath: "/var/run/docker.sock",
+});
+
 const useDocker = () => {
-    const docker = new Api({
-        baseURL: process.env.DOCKER_URL,
-        socketPath: '/var/run/docker.sock',
-    });
-
-    const getContainerState = async (id: string) => {
-        const { data: { State } } = await docker.containers.containerInspect(id);
-        if (!State) return;
-
-        return {
-            status: State.Status,
-            startDate: State.StartedAt,
-            finishDate: State.FinishedAt,
-        };
-    };
-
-    const containerAction = async (id: string, action: "start" | "stop" | "restart" ) => {
-        switch (action) {
-            case "start":
-                return await docker.containers.containerStart(id);
-            case "stop":
-                return await docker.containers.containerStop(id);
-            case "restart":
-                return await docker.containers.containerRestart(id);
-            default:
-                throw new Error("Invalid action: " + action);
-        }
-    };
+  const getContainerState = async (id: string) => {
+    const {
+      data: { State },
+    } = await docker.containers.containerInspect(id);
+    if (!State) return;
 
     return {
-        getContainerState,
-        containerAction,
+      status: State.Status,
+      startDate: State.StartedAt,
+      finishDate: State.FinishedAt,
+    };
+  };
+
+  const containerAction = async (
+    id: string,
+    action: "start" | "stop" | "restart"
+  ) => {
+    switch (action) {
+      case "start":
+        return await docker.containers.containerStart(id);
+      case "stop":
+        return await docker.containers.containerStop(id);
+      case "restart":
+        return await docker.containers.containerRestart(id);
+      default:
+        throw new Error("Invalid action: " + action);
     }
+  };
+
+  return {
+    getContainerState,
+    containerAction,
+  };
 };
 
 export default useDocker;
